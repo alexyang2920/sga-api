2ad0cf802fbc06a532a7e1b836e5b71bd84100a8
Alex Yang <zpyang2920@gmail.com>
2024-08-04 00:25:26 -0500

    rename routers to auth

--- sga/main.py
+++ sga/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from .routers.auth import router as authRouter
+from .auth.router import router as authRouter
 from .users.router import router as usersRouter
 from .roles.router import router as rolesRouter
 from .events.router import router as eventsRouter
