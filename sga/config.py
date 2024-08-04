32cc9e643084b0f556cd6029cb3061de49656eff
Alex Yang <alexyang@alexs-mbp.attlocal.net>
2024-08-04 16:01:27 -0500

    add .env.development

--- sga/config.py
+++ sga/config.py
@@ -1,16 +1,18 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    # database_url: str = 'sqlite+aiosqlite:///sga_app.db'
-    database_url: str = 'postgresql+asyncpg://sga:Temp1234@localhost:5432/sga'
+    database_url: str
 
-    # To get a string like this run: openssl rand -hex 32
-    jwt_secret_key: str = '10c6b69df90c84ed7253106f928c39553141ad644a61c28a059251ea1763f8b3'
-    jwt_algo: str = 'HS256'
-    jwt_access_token_expires_minutes: int = 24 * 60
+    jwt_secret_key: str
+    jwt_algo: str
+    jwt_access_token_expires_minutes: int
 
     model_config = SettingsConfigDict()
+
+    class Config:
+        env_file = os.getenv('ENV_FILE', '.env.development')
 
 
 settings = Settings()
